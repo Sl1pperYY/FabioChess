43d0996cf6fc0ed4daa8ee48f87c4127e76bc0e2
Sl1pperYY <32176695+Sl1pperYY@users.noreply.github.com>
2019-03-23 22:39:08 +0100

    Making only legal moves

--- NEA Project/Javascript Program/js/gui.js
+++ NEA Project/Javascript Program/js/gui.js
@@ -7,10 +7,12 @@ $("#SetFen").click(function() {
 // Board Set up
 //================================================================================
 
+// Function to clear all the pieces on the board
 function ClearAllPieces() {
     $(".piece").remove();
 }
 
+// Function to add a piece on the board
 function AddGUIPiece(sq, pce) {
 
 	var file = FileChar[FilesBrd[sq]];
@@ -31,6 +33,7 @@ function AddGUIPiece(sq, pce) {
     $(id).removeClass("square");
 }
 
+// Function to add all the pieces to the board
 function SetInitialBoardPieces() {
 
 	var sq;
@@ -48,6 +51,7 @@ function SetInitialBoardPieces() {
 	}
 }
 
+// Function to reposition all the pieces in the right location given by an FEN string
 function NewGame(fenStr) {
     ParseFen(fenStr);
     PrintBoard();
@@ -58,20 +62,19 @@ function NewGame(fenStr) {
 // Drag and Drop
 //================================================================================
 
-
-function allowDrop(ev) {
-    ev.preventDefault();
-}
-
 function drag(ev) {
     ev.dataTransfer.effectAllowed = 'move';
     ev.dataTransfer.setData("text", ev.target.id);
+    // ev.currentTarget.style.boxShadow = '0 0 0 0.2rem cyan';
     var pce = ev.target.id;
     pce = pce[0] + pce[1];
 
-    $(ev.target.id).children().addClass('.invisible');
+    SetFromSq($(ev.target).parent().attr('id'));
+
+    // $(ev.target).addClass('.selected');
     var img = new Image();
     
+    // Sets the correct drag image
     if (pce[1] == '7') {
         img = bPImg;
         ev.dataTransfer.setDragImage(img, 50, 50);
@@ -109,23 +112,59 @@ function drag(ev) {
         img = wKImg;
         ev.dataTransfer.setDragImage(img, 0, 0);
     }
-    
-
 }
   
 function drop(ev) {
-    if ($(ev.currentTarget).children().length == 0) {
+    // ev.currentTarget.childNode.style.boxShadow = '';
+    SetToSq($(ev.target).attr('id'));
+
+    var parsed = ParseMove(UserMove.from, UserMove.to);
+
+    if(parsed != NOMOVE && $(ev.currentTarget).children().length == 0) {
+        MakeMove(parsed);
+        
         ev.preventDefault();
         var data = ev.dataTransfer.getData("text");
         ev.currentTarget.appendChild(document.getElementById(data));
+    } else if(parsed != NOMOVE && $(ev.currentTarget).children().length == 1) {
+        MakeMove(parsed);
+        
+        ev.currentTarget.removeChild();
+        ev.preventDefault();
+        var data = ev.dataTransfer.getData("text");
+        ev.currentTarget.appendChild(document.getElementById(data));
+    } else {
+        console.log('Illegal Move')
     }
+}
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+// Function to set the square which the piece is being dragged from
+function SetFromSq(sq) {
+    file = (sq.charCodeAt(0) - 97);
+    rank = (parseInt(sq[1], 10) - 1);
+    coordinate = FR2SQ(file,rank);
+    UserMove.from = coordinate;
+}
+
+// Function to set the square which the piece is being moved to
+function SetToSq(sq) {
+    console.log(sq); 
+    file = (sq.charCodeAt(0) - 97);
+    rank = (parseInt(sq[1], 10) - 1);
+    coordinate = FR2SQ(file,rank);
+    UserMove.to = coordinate;
 }
 
 
 //================================================================================
-// Moving with clicking
+// Move Detection
 //================================================================================
 
+/*
 $(document).on('click','.piece', function (e) {
     console.log('Piece Click');
 });
@@ -133,3 +172,4 @@ $(document).on('click','.piece', function (e) {
 $(document).on('click','.square', function (e) {
     console.log('Square Click');
 });
+*/
